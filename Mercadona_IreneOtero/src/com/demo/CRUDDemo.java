26e6272da2fcd409d56a06e6e5b4108b412d0368
IreneOtero <irotdia@inf.upv.es>
2022-01-21 17:23:15 +0100

    BD Añadida

--- Mercadona_IreneOtero/src/com/demo/CRUDDemo.java
+++ Mercadona_IreneOtero/src/com/demo/CRUDDemo.java
@@ -15,7 +15,7 @@ import com.model.Producto;
 
 public class CRUDDemo {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		
 		try {
 			HashMap<String,Object> connectResult = Conexion.conectar();
